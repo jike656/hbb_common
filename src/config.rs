25db25e541e3e2193dfb742cfdadb02b71368fb6
jike656 <3095109528@qq.com>
2026-07-11 02:41:48 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -117,8 +117,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["122.152.204.140"];
+pub const RS_PUB_KEY: &str = "ruY4MgJj28lpDEthV97rZA6cyrfoqItFafRMW0uWED4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
